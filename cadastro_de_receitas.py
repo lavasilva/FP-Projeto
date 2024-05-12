c9171ca2106fa2005d0dd3695df5f3c523b54fbd
Vinícius Marques <viniciusboz1000@outlook.com>
2024-05-12 18:49:56 -0300

    Cadastro está praticamente pronto. Falta agora o CRUD

--- cadastro_de_receitas.py
+++ cadastro_de_receitas.py
@@ -1,29 +1,65 @@
-print("Cadastre aqui suas receitas por favor")
-receita = []
+def cadastro_receitas():
+    entrada = []
+    principal = []
+    sobremesa = []
 
-
-
-# Função de cadastro de receitas: tipo_de_prato indica a refeição
-
-def cadastro_receitas(receitas):
-     tipo = int(input('''Tipo de pratos:
-        1 - Entrada
+    while True:
+        print("Cadastro de Receitas")
+        selecao_prato = int(input('''
+        1 - Prato de Entrada
         2 - Prato Principal
         3 - Sobremesa
-        Selecione: '''))     
-     nome = input("Informe o nome da receita: ")
-     pais = input("Informe o país de origem: ")
-     ingredientes = input("Informe os ingredientes necessários: ").split('*')
-     preparo = input("Descreva o modo de preparo: ")
-     return receita
-     receita = {
-          "nome" : nome,
-          "País de Origem" : pais,
-          "ingredientes" : ingredientes,
-          "Modo de Preparo" : preparo
-     }
-     receita.append(receitas)
-     print("Receita cadastrada com sucesso!")
+        4 - Quantidade de receitas cadastradas                          
+        5 - Mostrar a lista
+        6 - Sair
+        
+        Selecione: '''))
 
+        if selecao_prato == 4:
+            print("Quantidade de receitas cadastradas:")
+            print(f"Entrada: {len(entrada)} receitas")
+            print(f"Principal: {len(principal)} receitas")
+            print(f"Sobremesa: {len(sobremesa)} receitas")
+            print()
 
+        elif selecao_prato == 5:
+            print("Receitas cadastradas:")
+            for tipo, receitas in [("Entrada", entrada), ("Principal", principal), ("Sobremesa", sobremesa)]:
+                print(f"{tipo}: {len(receitas)} receitas")
+                for receita in receitas:
+                    print(receita)
+            print()
 
+        elif selecao_prato == 6:
+            print("Tenha um bom dia!")
+            break
+
+        else:
+            nome = input("\nInforme o nome do prato: ")
+            pais = input("Informe o país de origem do prato: ")
+            ingredientes = input("Informe os ingredientes para o prato: ")
+            preparo = input("Escreva o modo de preparo: ")
+
+            receita = {
+                "Nome": nome,
+                "País de origem": pais,
+                "Ingredientes": ingredientes,
+                "Modo de preparo": preparo
+            }
+
+            if selecao_prato == 1:
+                print("É uma entrada\n")
+                entrada.append(receita)
+
+            elif selecao_prato == 2:
+                print("É um prato principal\n")
+                principal.append(receita)
+
+            elif selecao_prato == 3:
+                print("É uma sobremesa\n")
+                sobremesa.append(receita)
+
+            else:
+                print("Opção inválida!")
+
+cadastro_receitas()
